9466bf24bc36fcff8644f33f9bd660c7fee74174
Billlllllllllllllly <2305115@stu.chi.ac.uk>
2024-10-04 14:28:23 +0100

    assets and ai added

---
+++ Assets/scripts/enemyai.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyai : MonoBehaviour
+{
+
+    [SerializeField] Transform target; 
+    NavMeshAgent nMA;
+
+    float aggroRange = 5f;
+    float distanceToPlayer; 
+
+    void Start()
+    {
+        nMA = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        distanceToPlayer = Vector3.Distance(target.position, transform.position);
+
+        if(distanceToPlayer <= aggroRange) 
+        {
+            nMA.SetDestination(target.transform.position);
+        }
+    }
+}
