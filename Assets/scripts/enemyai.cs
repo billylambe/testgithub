361bb55d645fd4154960b5c8d7be436581e3f750
Billlllllllllllllly <2305115@stu.chi.ac.uk>
2024-10-07 15:32:31 +0100

    GUN AND ENEMY THE GTA UPDATE CAN RIDE ENEMY LIKE HORSE

--- Assets/scripts/enemyai.cs
+++ Assets/scripts/enemyai.cs
@@ -8,9 +8,17 @@ public class enemyai : MonoBehaviour
 
     [SerializeField] Transform target; 
     NavMeshAgent nMA;
+   
 
     float aggroRange = 5f;
-    float distanceToPlayer; 
+    float distanceToPlayer;
+    float attackRange = 2f;
+    float turnSpeed = 5f;
+
+    bool isAggro = false;
+    bool attackState = false;
+    bool chasePlayer = false;
+    
 
     void Start()
     {
@@ -21,10 +29,31 @@ public class enemyai : MonoBehaviour
     private void Update()
     {
         distanceToPlayer = Vector3.Distance(target.position, transform.position);
+        if (distanceToPlayer <= aggroRange) chasePlayer = true;
+
+        if (chasePlayer);
+        {
+            
+            nMA.SetDestination(target.transform.position);
+            if (distanceToPlayer <= attackRange) attackState = true;
+            else chasePlayer = true;
+
+        }
 
         if(distanceToPlayer <= aggroRange) 
         {
             nMA.SetDestination(target.transform.position);
         }
+
+        
+    }
+
+    void RotateToPlayer()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion currentRotation = transform.rotation;
+        Quaternion desiredRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+        Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * turnSpeed);
     }
 }
---
+++ Assets/scripts/gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    [SerializeField] Camera playerCam;
+    [SerializeField] float raycastRange = 100f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) shoot();
+        
+
+    }
+
+    private void shoot()
+    {
+        RaycastHit Hitinfo;
+        RaycastHit somethingHit;
+        Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out Hitinfo, raycastRange);
+
+    }
+}
